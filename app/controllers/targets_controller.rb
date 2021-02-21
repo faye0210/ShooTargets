673ce8ce304c1564471465914db3ff5054122a3e
Faye.H <yang5chen12hui@gmail.com>
2021-02-21 16:37:22 +0900

    ターゲットの大まかな機能（一覧、詳細、作成、編集、削除）の実装

--- app/controllers/targets_controller.rb
+++ app/controllers/targets_controller.rb
@@ -1,2 +1,55 @@
 class TargetsController < ApplicationController
+  before_action :set_target, only: %i[ show edit update destroy]
+  # before_action :ensure_correct_user, only: %i[edit update destroy]
+
+  def index
+    @targets = Target.all
+  end
+
+  def show; end
+
+  def new
+    @target = Target.new
+  end
+
+  def create
+    @target = Target.new(target_params)
+    if params[:back]
+      render :new
+    else
+      if @target.save
+        redirect_to targets_path, notice: ""
+      else
+        render :new
+      end
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @blog.update(target_params)
+      redirect_to targets_path, notice: ""
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @target.destroy
+    redirect_to targets_path, notice: ""
+  end
+
+  private
+  def set_target
+    @target = Target.find(params[:id])
+  end
+
+  def target_params
+    params.require(:target).permit(:title, :detail, :deadline)
+  end
+
+  # def ensure_correct_user
+  #
+  # end
 end
--- app/models/target.rb
+++ app/models/target.rb
@@ -1,2 +1,4 @@
 class Target < ApplicationRecord
+  validates :title, presence: true, length: { in: 1..255 }
+  validates :deadline, presence: true
 end
