1f452eaa7d17140751b882680a168cf8b917645b
Faye.H <yang5chen12hui@gmail.com>
2021-02-24 16:08:17 +0900

    メール認証を実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:login]
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable, :authentication_keys => [:login]
 
   def self.find_first_by_auth_conditions(warden_conditions)
    conditions = warden_conditions.dup
