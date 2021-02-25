2bf4215a5abf9fdac817b2cb8a36ad5eb0621c41
Faye.H <yang5chen12hui@gmail.com>
2021-02-25 11:01:53 +0900

    ログイン部分の修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  root to: 'oauth_test#index'
   devise_for :users, controllers: {
     registrations: "users/registrations",
     omniauth_callbacks: "users/omniauth_callbacks"
--- test/controllers/oauth_test_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class OauthTestControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get oauth_test_index_url
-    assert_response :success
-  end
-
-end
