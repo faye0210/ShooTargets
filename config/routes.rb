aec9dff38477b0bdad962bb06cf69f5a01533163
Faye.H <yang5chen12hui@gmail.com>
2021-02-26 00:33:47 +0900

    ルーティングの変更

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,13 @@ Rails.application.routes.draw do
     registrations: "users/registrations",
     omniauth_callbacks: "users/omniauth_callbacks"
   }
+
   resources :users, only: [:show, :edit]
 
-  root to: "targets#index"
   resources :targets do
     member do
       post "achieve"
     end
   end
+  root to: "targets#index"
 end
